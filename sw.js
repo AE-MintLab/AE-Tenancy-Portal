8615360bda83713b6c6cd43f47e656c6f700d128
ayubewan-tech <ayubewan@gmail.com>
2026-09-03 13:53:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,72 +7,29 @@ const APP_FILES = [
     './logo.png'
 ];
 
-self.addEventListener('install', (event) => {
-
+self.addEventListener('install', event => {
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-
-            .then((cache) => {
-                return cache.addAll(APP_FILES);
-            })
-
-            .then(() => {
-                return self.skipWaiting();
-            })
-
+            .then(cache => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
     );
-
 });
 
-
-self.addEventListener('activate', (event) => {
-
+self.addEventListener('activate', event => {
     event.waitUntil(
-
-        caches.keys()
-
-            .then((cacheNames) => {
-
-                return Promise.all(
-
-                    cacheNames
-
-                        .filter(
-                            (name) => name !== CACHE_NAME
-                        )
-
-                        .map(
-                            (name) => caches.delete(name)
-                        )
-
-                );
-
-            })
-
-            .then(() => {
-
-                return self.clients.claim();
-
-            })
-
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
     );
-
 });
 
-
-self.addEventListener('fetch', (event) => {
-
+self.addEventListener('fetch', event => {
     event.respondWith(
-
         fetch(event.request)
-
-            .catch(() => {
-
-                return caches.match(event.request);
-
-            })
-
+            .catch(() => caches.match(event.request))
     );
-
 });
