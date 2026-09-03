4b1c71e00eb8386f6f725767a37e003f3ef41495
ayubewan-tech <ayubewan@gmail.com>
2026-09-03 13:08:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,35 @@
-// Simple offline service worker for PWA support
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'ae-tenancy-portal-v1';
+
+const APP_FILES = [
+    './',
+    './index.html',
+    './manifest.json',
+    './logo.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Let network handle requests standardly
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => caches.match(event.request))
+    );
 });
